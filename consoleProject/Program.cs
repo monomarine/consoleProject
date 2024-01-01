ad37c80ebe7abc06aa91289062387e9521e48c55
egurnova elena <gamedev409@yandex.ru>
2024-01-01 22:13:21 +0500

    Добавьте файлы проекта.

---
+++ consoleProject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace consoleProject
+{
+    /*
+     * ФИО студента: 
+     * номер варианта: 
+     * условие задачи (скопировать из листа задания): 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
